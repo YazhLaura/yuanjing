23c800714d05a4b3abed31cfa16172dfc9e6f45b
YazhLaura <hyz1206ato@gmail.com>
2025-03-01 17:26:43 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VisionBoardCreator from './components/VisionBoardCreator';
+
+function App() {
+  return (
+    <div className="App">
+      <VisionBoardCreator />
+    </div>
+  );
+}
+
+export default App;
